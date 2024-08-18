0a6362a9387545394462cc861efcdc720a80e107
mar-alv <mar.alv.cod@gmail.com>
2024-08-18 14:16:23 -0300

    feat: tailwind and shadcn added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config'
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mar-alv.github.io'
+  site: 'https://mar-alv.github.io',
+  integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false
+		})
+	]
 })
