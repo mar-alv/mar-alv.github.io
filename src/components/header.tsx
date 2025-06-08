e80a728e286e972567f342aba166cd0c4e4d2235
mar-alv <mdsantos.alvarez@gmail.com>
2025-06-08 10:18:55 -0300

    feat: adding TODOs and FIXMEs

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -53,6 +53,8 @@ export function Header({ currentPath }: Props) {
           ))}
         </NavigationMenuList>
       </NavigationMenu>
+
+      {/* TODO: add language select */}
     </header>
   )
 }
